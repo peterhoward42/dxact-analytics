6bfed0383a7d8534bf6ab0879e6c09abbce2dffb
peterhoward42 <peterhoward42@gmail.com>
2025-11-05 17:51:24 +0000

    Capture flags for which events received per user

--- analysis/lib/visitors/visitorimplementations/simplecounter.go
+++ analysis/lib/visitors/visitorimplementations/simplecounter.go
@@ -7,27 +7,73 @@ import (
 
 // SimpleCounter IS-A visitor.Visitor implementation that does a few trivial counts. (To get this process going)
 type SimpleCounter struct {
-	// Users captures all the unique user proxy ids found in the set of events.
-	Users map[string]bool
 
-	// All unique event types encountered.
-	EventTypes map[string]bool
+	// userBehaviours captures event counts and flags on a per user basis.
+	// It is keyed on a user's proxyID.
+	userBehaviours map[string]*UserBehaviour
 }
 
 func NewSimpleCounter() *SimpleCounter {
 	return &SimpleCounter{
-		Users: map[string]bool{},
-		EventTypes: map[string]bool{},
+		userBehaviours: map[string]*UserBehaviour{},
 	}
 }
 
 func (sc *SimpleCounter) Visit(event lib.EventPayload, path string) (err error) {
-	// Accumulate the user proxy ID's encountered.
-	sc.Users[event.ProxyUserID] = true
-	sc.EventTypes[event.Event] = true
+	// Initialise the per-user behaviour record the first time we encounter a user.
+	var thisUsersBehaviour *UserBehaviour
+	var ok bool
+	if thisUsersBehaviour, ok = sc.userBehaviours[event.ProxyUserID]; !ok {
+		thisUsersBehaviour = NewUserBehaviour()
+		sc.userBehaviours[event.ProxyUserID] = thisUsersBehaviour
+	}
+
+	switch {
+
+	case event.Event == "evt:launched":
+		thisUsersBehaviour.Launched = true
+
+	case event.Event == "evt:enter-training-cage":
+		thisUsersBehaviour.EnteredTrainingCage = true
+
+	case event.Event == "evt:training-completed":
+		thisUsersBehaviour.CompletedTrainingCage = true
+
+	case event.Event == "evt:sign-in-started":
+		thisUsersBehaviour.SignInStarted = true
+
+	case event.Event == "evt:sign-in-success":
+		thisUsersBehaviour.SignInSucceeded = true
+
+	case event.Event == "evt:loaded-example":
+		thisUsersBehaviour.LoadedAnExample = true
+
+	case event.Event == "evt:created-new-drawing":
+		thisUsersBehaviour.CreatedNewDrawing = true
+
+	case event.Event == "evt:retreived-save-drawing":
+		thisUsersBehaviour.ReteivedSavedDrawing = true
+
+	}
+
 	return
 }
 
 func (sc *SimpleCounter) Report() string {
-	return litter.Sdump(sc)
+	return litter.Sdump(sc.userBehaviours)
+}
+
+type UserBehaviour struct {
+	Launched              bool
+	EnteredTrainingCage   bool
+	CompletedTrainingCage bool
+	LoadedAnExample       bool
+	SignInStarted         bool
+	SignInSucceeded       bool
+	CreatedNewDrawing     bool
+	ReteivedSavedDrawing  bool
+}
+
+func NewUserBehaviour() *UserBehaviour {
+	return &UserBehaviour{}
 }
